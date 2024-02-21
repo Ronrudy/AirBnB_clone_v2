7625e31f2a0e05b67119201e3a5c18e5ad1672e6
Ronrudy <ruffroony@yahoo.com>
2024-02-21 15:43:09 -0800

    updating file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,7 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-         if cls is None:
+        if cls is None:
             return self.__objects
         cls_name = cls.__name__
         dct = {}
